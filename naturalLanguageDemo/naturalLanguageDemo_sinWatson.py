4dfef726910132c6b132a4fa17f7d6e59da30656
miguelMontiel <miguelmontielgalicia@hotmail.com>
2017-11-15 12:59:34 -0600

    Add files via upload

--- naturalLanguageDemo/naturalLanguageDemo_sinWatson.py
+++ naturalLanguageDemo/naturalLanguageDemo_sinWatson.py
@@ -1,24 +1,25 @@
-archivo = open("archivo.txt")
-archivoNuevo = open("archivoNuevo.txt", "w")
+import tkinter.filedialog as Tkinter
+import os.path
+
+inicio = "----------- STRING(S) FOUND -------------------"
+
+archivo = Tkinter.askopenfile(title = "Archivos TXT")
+archivoNew = 'C:/Users/IBM_ADMIN/Desktop/archivosLaYessenia'
+#NewfileChido = os.path.basename(archivo)
+
+print(archivo)
 
 for linea in archivo:
-    if '-------' in linea:
-        print(linea)
-        archivoNuevo.write("\n")
-        archivoNuevo.write(linea)
+    if inicio in linea:
+        tablaTitulo = linea[2:10]
+        print(tablaTitulo)
+        with open(archivoNew + archivo) as archivoNuevo:
+            archivoNuevo.write("\n")
+            archivoNuevo.write(tablaTitulo)
 
     for palabra in linea.split():
-        if palabra == "INCLUDE":
-            print(linea)
-            archivoNuevo.write(linea)
-
-        if palabra == "FROM":
-            print(linea)
-            archivoNuevo.write(linea)
-
-        if palabra == "MOVE":
-            print(linea)
-            archivoNuevo.write(linea)
-
+        if palabra == "FROM" or palabra == "MOVE" or palabra == "PIC":
+            print(".")
+            #blahhh.write(".")
 archivo.close()
-archivoNuevo.close()
+#blahhh.close()
