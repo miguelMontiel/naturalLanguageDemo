8232239add93b0367597e6aed8ce6bdb931da1ae
miguelMontiel <miguelmontielgalicia@hotmail.com>
2017-11-16 02:06:57 -0600

    Add files via upload

--- naturalLanguageDemo/naturalLanguageDemo_sinWatson.py
+++ naturalLanguageDemo/naturalLanguageDemo_sinWatson.py
@@ -4,20 +4,37 @@ import os.path
 inicio = "----------- STRING(S) FOUND -------------------"
 
 archivo = Tkinter.askopenfilename(initialdir="C:/Users/mainUser/PycharmProjects/naturalLanguageDemo", title="Archivos TXT")
-archivoNuevo = "C:/Users/mainUser/Desktop/laYessenia/" + "Chido-" + os.path.basename(str(archivo))
+temp = "C:/Users/mainUser/Desktop/laYessenia/" + "temp.txt"
 
 archivos = open(archivo, "r")
-archivoNuevos = open(archivoNuevo, "w+")
+archivoNuevo = open(temp, "w+")
 
 for linea in archivos:
     if inicio in linea:
         tablaTitulo = linea[2:10]
-        print(tablaTitulo)
-        archivoNuevos.write("\n")
-        archivoNuevos.write(tablaTitulo + " ")
+        archivoNuevo.write("\n")
+        archivoNuevo.write(tablaTitulo + " ")
 
     for palabra in linea.split():
         if palabra == "FROM" or palabra == "MOVE" or palabra == "PIC":
-            archivoNuevos.write(".")
+            archivoNuevo.write(".")
+
 archivos.close()
-archivoNuevos.close()
+archivoNuevo.close()
+
+
+archivoNuevo2 = "C:/Users/mainUser/Desktop/laYessenia/" + "Chido-" + os.path.basename(str(archivo))
+
+archivos2 = open("C:/Users/mainUser/Desktop/laYessenia/temp.txt", "r")
+archivoFinal = open(archivoNuevo2, "w+")
+
+for linea2 in archivos2:
+    if "." in linea2:
+        ayuda = linea2.replace(".", "")
+        print("Si tiene: ", ayuda)
+        archivoFinal.write(ayuda)
+    else:
+        print("No tiene: ", linea2)
+
+archivos2.close()
+archivoFinal.close()
