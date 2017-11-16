456eba84453788a5d06fc58d80e93492d511c015
miguelMontiel <miguelmontielgalicia@hotmail.com>
2017-11-16 13:51:06 -0600

    Add files via upload

--- naturalLanguageDemo/naturalLanguageDemo_sinWatson.py
+++ naturalLanguageDemo/naturalLanguageDemo_sinWatson.py
@@ -3,38 +3,37 @@ import os.path
 
 inicio = "----------- STRING(S) FOUND -------------------"
 
-archivo = Tkinter.askopenfilename(initialdir="C:/Users/mainUser/PycharmProjects/naturalLanguageDemo", title="Archivos TXT")
-temp = "C:/Users/mainUser/Desktop/laYessenia/" + "temp.txt"
+archivo = Tkinter.askopenfilenames(initialdir="C:/Users/IBM_ADMIN/Desktop/archivosOriginales/", title="Archivos TXT")
+temp = "C:/Users/IBM_ADMIN/Desktop/archivosLaYessenia/temp.txt"
 
-archivos = open(archivo, "r")
-archivoNuevo = open(temp, "w+")
+for individuales in archivo:
+    archivos = open(individuales, "r")
+    archivoNuevo = open(temp, "w+")
 
-for linea in archivos:
-    if inicio in linea:
-        tablaTitulo = linea[2:10]
-        archivoNuevo.write("\n")
-        archivoNuevo.write(tablaTitulo + " ")
+    for linea in archivos:
+        if inicio in linea:
+            tablaTitulo = linea[2:10]
+            archivoNuevo.write("\n")
+            archivoNuevo.write(tablaTitulo + " ")
 
-    for palabra in linea.split():
-        if palabra == "FROM" or palabra == "MOVE" or palabra == "PIC":
-            archivoNuevo.write(".")
+        for palabra in linea.split():
+            if palabra == "FROM" or palabra == "MOVE" or palabra == "PIC":
+                archivoNuevo.write(".")
 
-archivos.close()
-archivoNuevo.close()
+    archivos.close()
+    archivoNuevo.close()
 
 
-archivoNuevo2 = "C:/Users/mainUser/Desktop/laYessenia/" + "Chido-" + os.path.basename(str(archivo))
+    archivoNuevo2 = "C:/Users/IBM_ADMIN/Desktop/archivosLaYessenia/" + "Chido-" + os.path.basename(str(individuales))
 
-archivos2 = open("C:/Users/mainUser/Desktop/laYessenia/temp.txt", "r")
-archivoFinal = open(archivoNuevo2, "w+")
+    archivos2 = open("C:/Users/IBM_ADMIN/Desktop/archivosLaYessenia/temp.txt", "r")
+    archivoFinal = open(archivoNuevo2, "w+")
 
-for linea2 in archivos2:
-    if "." in linea2:
-        ayuda = linea2.replace(".", "")
-        print("Si tiene: ", ayuda)
-        archivoFinal.write(ayuda)
-    else:
-        print("No tiene: ", linea2)
+    for linea2 in archivos2:
+        if "." in linea2:
+            ayuda = linea2.replace(".", "")
+            archivoFinal.write(ayuda)
 
-archivos2.close()
-archivoFinal.close()
+    print(individuales)
+    archivos2.close()
+    archivoFinal.close()
